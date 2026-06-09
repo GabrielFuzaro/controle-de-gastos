0845643f7fd1ede63f9a242166af98e2c861d048
Gabriel Fuzaro <gabrielbbfuzaro@gmail.com>
2026-06-09 15:04:38 -0300

    n entendi porra nenhuma mas ta funcionando

--- Actions/salvar_gasto.php
+++ Actions/salvar_gasto.php
@@ -1,11 +1,26 @@
 <?php 
 
+require_once '../config/conexao.php';
+require_once '../Models/Gasto.php';
+require_once '../Repositories/GastoRepository.php';
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
     $descricao = trim($_POST['descricao']);
     $categoria = trim($_POST['categoria']);
     $valor = (float) $_POST['valor'];
     $data = $_POST['data'];
+
+    $gasto = new Gasto($descricao, $categoria, $valor, $data);
+
+    $repostitory = new GastoRepository($conn);
+
+    if($repostitory->salvar($gasto)){
+        header("Location: ../views/index.php");
+        exit;
+    }
+
+    echo "Erro ao salvar gasto";
 
 }
 
--- Repositories/GastoRepository.php
+++ Repositories/GastoRepository.php
@@ -1,5 +1,31 @@
-<?php 
+<?php
 
+require_once __DIR__ . '/../Models/Gasto.php';
 
+class GastoRepository
+{
+    private $conn;
 
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
+    }
+
+    public function salvar(Gasto $gasto)
+    {
+        $stmt = $this->conn->prepare("INSERT INTO gastos (descricao, categoria, valor, data_gasto)
+            VALUES (?, ?, ?, ?)
+        ");
+
+        $stmt->bind_param(
+            "ssds",
+            $gasto->descricao,
+            $gasto->categoria,
+            $gasto->valor,
+            $gasto->data_gasto
+        );
+
+        return $stmt->execute();
+    }
+}
 ?>
