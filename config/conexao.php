1af31b74a103615aa3b704e58a8d69ddfab75223
Gabriel Fuzaro <gabrielbbfuzaro@gmail.com>
2026-06-17 17:38:30 -0300

    mudanca na conexao

--- config/conexao.php
+++ config/conexao.php
@@ -2,7 +2,7 @@
 
 function getConnection(){
     try{
-        $pdo = new PDO("mysql:host=localhost;dbname=controle_gastos;charset=utf8", "root", "");
+        $pdo = new PDO("mysql:host=localhost;dbname=controle_gastos;charset=utf8mb4", "root", "");
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
