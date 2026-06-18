674612a5277ae73b8745c277447c3d8b26d42483
Gabriel Fuzaro <gabrielbbfuzaro@gmail.com>
2026-06-18 16:31:08 -0300

    Validação melhorada

--- Actions/salvar_extras.php
+++ Actions/salvar_extras.php
@@ -8,17 +8,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
     $conn = getConnection();
 
-    $descricao = trim($_POST['descricao']);
-    $valor = (float) $_POST['valor'];
-    $data = $_POST['data_entrada'];
-  
+    $descricao = trim($_POST['descricao'] ?? '');
+    $valor = (float) trim($_POST['valor'] ?? '');
+    $data = trim($_POST['data_entrada']);
+
+    if($descricao === '' || $valor === false || $valor<=0 || $data === ''){
+        $_SESSION['erro'] = 'Preencha todos os campos corretamente!';
+        header("Location: ../views/cadastrar_gasto.php");
+    }else{
+
     $extra = new Extra($descricao, $valor, $data);
 
     $repository_extra = new ExtraRepository($conn);
 
-    if($repository_extra->salvar($extra)){
-        header("Location: ../views/dashboard.php");
-        exit;
+        if($repository_extra->salvar($extra)){
+            header("Location: ../views/dashboard.php");
+            exit;
+        }
+
+    $_SESSION['erro'] = 'Erro ao cadastrar entrada.';
+    header("Location: ../Views/cadastrar_gasto.php");
+    exit;
     }
 }
 
--- Actions/salvar_gasto.php
+++ Actions/salvar_gasto.php
@@ -8,10 +8,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
     $conn = getConnection();
 
-    $descricao = trim($_POST['descricao']);
-    $categoria = trim($_POST['categoria']);
+    $descricao = trim($_POST['descricao'])?? '';
+    $categoria = trim($_POST['categoria'])?? '';
     $valor = (float) $_POST['valor'];
-    $data = $_POST['data'];
+    $data = $_POST['data']?? '';
 
     if (strlen($descricao) < 3) {
     die("A descrição deve ter pelo menos 3 caracteres.");
@@ -28,7 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     'Presentes'
     ];
 
-    if (!in_array($categoria, $categoriasPermitidas)) {
+    if (!in_array($categoria, $categoriasPermitidas, true)) {
         die("Categoria inválida.");
     }
 
